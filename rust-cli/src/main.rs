54fcad22e69892cb0543e82c339316875c4d0e89
Sara <saraharris2000@gmail.com>
2023-08-16 21:11:22 -0700

    updates to main.rs

--- rust-cli/src/main.rs
+++ rust-cli/src/main.rs
@@ -3,18 +3,37 @@ use clap::{arg, Command};
 fn main() {
     let matches = Command::new("MyApp")
         .version("1.0")
-        .author("Kevin K. <kbknapp@gmail.com>")
-        .about("Does awesome things")
-        .arg(arg!(--two <VALUE>).required(true))
-        .arg(arg!(--one <VALUE>).required(true))
+        .author("Laconics <laconics@protonmail.com>")
+        .about("Does weird shiznit")
+        .arg(arg!(--name <VALUE>).required(true))
+        .arg(arg!(--iq <VALUE>).required(true))
         .get_matches();
 
-    println!(
-        "two: {:?}",
-        matches.get_one::<String>("two").expect("required")
-    );
-    println!(
-        "one: {:?}",
-        matches.get_one::<String>("one").expect("required")
-    );
+    //println!(
+    //    "Your stupid name: {:?}",
+    //    matches.get_one::<String>("name").expect("required")
+    //);
+    //println!(
+    //    "Wow thats such a low iq: {:?}",
+    //    matches.get_one::<String>("iq").expect("required")
+    //);
+
+    let name = matches.get_one::<String>("name").expect("required");
+    let iq = matches.get_one::<i32>("iq").expect("required");
+
+    println!("Iq is: {}", iq);
+
+    let otheriq = 10;
+
+    make_sentence(name, &otheriq);
+}
+
+fn make_sentence(name: &String, iq: &i32) {
+    println!("Yooo what is up {name}. Lemme read your mind!!!");
+    if *iq > 100 as i32 {
+        println!("Wow you're so smart!!! IQ: {}", iq)
+    } else {
+        println!("Woooof that's a ruff one!!")
+    }
+    //println!("Hmmmm you're IQ is so low!")
 }
