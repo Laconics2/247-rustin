aecdb664cdcaa8e1bd654d9cd4d8066fdeb5e9c9
Sara <saraharris2000@gmail.com>
2023-08-18 23:19:17 -0700

    Literally can't think rn but here's what I did

---
+++ single-number.rs
@@ -0,0 +1,55 @@
+
+fn main() {
+    // the main funciton
+    let _vec = vec![5,6,4,6,5];
+    let _vec2 = vec![4,1,2,1,2];
+    let _vec3 = vec![11, 0, 0, 3, 11];
+    println!("Non Double: {}", other_num(_vec));
+    println!("Non Double: {}", other_num(_vec2));
+    println!("Non Double: {}", other_num(_vec3))
+    
+}
+
+fn single_number(nums: Vec<i32>) -> i32 {
+    // takes in an array of numbers
+    // some numbers appear twice, find that one that appears once
+    let da_nums = nums;
+    let mut hold_arr = Vec::with_capacity(da_nums.len());
+    let mut ret_num = da_nums[0];
+
+    for i in 0..da_nums.len() {
+        println!("On loop: {}", i);
+        println!("Current num is: {}", da_nums[i]);
+        if !hold_arr.contains(&da_nums[i]) {
+            hold_arr.push(da_nums[i]);
+            ret_num = da_nums[i];
+        }
+    }
+    return ret_num;
+    
+}
+
+fn other_num(nums: Vec<i32>) -> i32 {
+    let da_nums = nums;
+    let mut current_winner_index = 0;
+    let mut seen_arr = Vec::with_capacity(da_nums.len());
+    seen_arr.push(da_nums[0]);
+
+    for i in 1..da_nums.len() {
+        //If the value at the current winner is equal to the value at i 
+        // then it can't be the value so we'll inc current winner position
+        // and also add
+    //    if da_nums[i] == da_nums[current_winner_index] || seen_arr.contains(&da_nums[i]) {
+    //        current_winner_index = current_winner_index + 1;
+    //    } else {
+    //        seen_arr.push(da_nums[i])
+    //    }
+
+        if !seen_arr.contains(&da_nums[i]) {
+            seen_arr.push(da_nums[i]);
+        } else if da_nums[current_winner_index] == da_nums[i] {
+            current_winner_index = current_winner_index +1;
+        }
+    }
+    return da_nums[current_winner_index];
+}
