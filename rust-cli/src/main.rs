39dcc39ba84913ad9fa7d92be1910f7d1131fcc0
Sara <saraharris2000@gmail.com>
2023-08-22 20:33:17 -0700

    add speak command, just takes a string and echos it

--- rust-cli/src/main.rs
+++ rust-cli/src/main.rs
@@ -5,14 +5,14 @@ use clap::{Parser, Subcommand};
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 struct Cli {
-    //optional name to operate on 
+    /// what's your name? .... TONY
     name: Option<String>,
 
-    //sets a custom config file
+    /// Stop FILE time
     #[arg(short, long, value_name = "FILE")]
     config: Option<PathBuf>,
 
-    //turn on debugging 
+    /// DEBUG more like LE BUG, hon hon hon french buggete
     #[arg(short, long, action = clap::ArgAction::Count)]
     debug: u8,
 
@@ -23,10 +23,18 @@ struct Cli {
 #[derive(Subcommand)]
 enum Commands {
     //does test things
-    //
+    /// Tests some stuff
     Test {
         #[arg(short, long)]
         list: bool,
+    },
+    
+    /// Shall I say something?
+    Speak {
+        #[arg(short, long)]
+        list: bool,
+        /// can I put something here?
+        something: String,
     },
 }
 
@@ -56,6 +64,13 @@ fn main() {
                 println!("Printin some testing lists....");
             } else {
                 println!("Not printin testing lists....");
+            }
+        }
+        Some(Commands::Speak {list, something}) => {
+            if * list {
+                println!("Shhhh! I am speaking! I want to say: {}!", something);
+            } else {
+                println!("Fine I won't say anything then, rat!");
             }
         }
         None => {}
